b0556f080db1f07d3b8f37ec33547567b2239873
johannes sandjaja <johannes@192.168.1.4>
2023-10-11 19:45:35 +0700

    error handling is kinda hard

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::SocketAddr;
 
-use axum::{middleware, Extension, Router};
+use axum::{http::StatusCode, middleware, response::IntoResponse, routing::get, Extension, Router};
 use sea_orm::{Database, DatabaseConnection};
 use tower_http::trace::{self, TraceLayer};
 use tracing::Level;
@@ -21,21 +21,32 @@ async fn server() {
     let db: DatabaseConnection = Database::connect(connection_string).await.unwrap();
     init_cron();
     tracing_subscriber::fmt().with_target(false).init();
+
     let app: Router = Router::new()
+        .route("/", get(|| async {}))
         .merge(routes::user::routes())
         .merge(routes::expenses::routes())
         .route_layer(middleware::from_fn(utils::guards::guard))
+        .merge(routes::expenses::routes())
         .merge(routes::auth::routes())
         .layer(Extension(db))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
                 .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-        );
+        )
+        .fallback(handler_404);
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap()
+}
+
+async fn handler_404() -> impl IntoResponse {
+    (
+        StatusCode::NOT_FOUND,
+        "The requested resource was not found",
+    )
 }
--- src/utils/api_error.rs
+++ src/utils/api_error.rs
@@ -1,15 +1,22 @@
-use axum::{http::{StatusCode, header}, response::IntoResponse, Json};
+use axum::{
+    http::{header, StatusCode},
+    response::IntoResponse,
+    Json,
+};
 use serde_json::json;
 
 pub struct APIError {
     pub message: String,
     pub status_code: StatusCode,
-    pub error_code: Option<i8>
+    pub error_code: Option<i8>,
 }
 
 impl IntoResponse for APIError {
     fn into_response(self) -> axum::response::Response {
         let status_code = self.status_code;
-        (status_code, [(header::CONTENT_TYPE, "application/json")], Json(json!({"StatusCode": self.status_code.as_u16(), "ErrorCode": self.error_code, "Message": self.message}))).into_response()
+        (status_code, 
+            [(header::CONTENT_TYPE, "application/json")],
+            Json(json!({"StatusCode": self.status_code.as_u16(), "ErrorCode": self.error_code, "Message": self.message}))
+        ).into_response()
     }
 }
